4f98e7628fc1c7f61c6314ea7afdf52a65d488a3
Dimidium <dimidium@dimidiummods.com>
2024-03-21 18:09:47 +0000

    Update DimidiumCore and add warning screen

--- src/main/java/net/dimidium/aboe/ABOE.java
+++ src/main/java/net/dimidium/aboe/ABOE.java
@@ -3,15 +3,19 @@ package net.dimidium.aboe;
 import com.mojang.logging.LogUtils;
 import net.dimidium.aboe.client.render.DisplayPedestalRenderer;
 import net.dimidium.aboe.client.screen.DisplayPedestalScreen;
+import net.dimidium.aboe.client.screen.WarningScreen;
 import net.dimidium.aboe.handler.ConfigurationHandler;
 import net.dimidium.aboe.handler.registry.*;
 import net.dimidium.aboe.event.CommandEvent;
 import net.dimidium.aboe.util.Constants;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -68,6 +72,24 @@ public class ABOE
                 ItemBlockRenderTypes.setRenderLayer(FluidRegistry.FLOWING_LIQUID_EXPERIENCE.get(), RenderType.translucent());
                 MenuScreens.register(ContainerRegistry.DISPLAY_PEDESTAL.get(), DisplayPedestalScreen::new);
             });
+        }
+    }
+
+    @Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE, modid = Constants.MOD_ID)
+    public static class ForgeEvents
+    {
+        private static boolean firstTitleScreenShown = false;
+
+        @SubscribeEvent
+        public static void showPreAlphaScreen(ScreenEvent.Init.Post event)
+        {
+            if (firstTitleScreenShown || !(event.getScreen() instanceof TitleScreen))
+            {
+                return;
+            }
+
+            Minecraft.getInstance().setScreen(new WarningScreen(event.getScreen()));
+            firstTitleScreenShown = true;
         }
     }
 }
